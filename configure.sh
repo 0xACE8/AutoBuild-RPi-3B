57e210a59e42ed0972e89a69b8174d0789f56a89
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2021-11-21 15:12:21 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,11 +2,17 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git dnscrypt-proxy2 https://github.com/jlzsss/dnscrypt-proxy2' >>feeds.conf.default
+
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon 
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
