0ccf0581892552074103de03565016018458626c
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2021-11-21 17:59:22 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git dnscrypt-proxy2 https://github.com/jlzsss/dnscrypt-proxy2' >>feeds.conf.default
+echo 'src-git ace https://github.com/0xACE8/ace-packages.git' >>feeds.conf.default
 
 
 # 替换默认主题
